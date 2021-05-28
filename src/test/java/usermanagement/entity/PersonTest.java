01154bbea9c0c5527a2cfc08a0e179c4e02b30ae
hassan <h.zaker2222@gmail.com>
2021-05-28 22:52:46 +0430

    experiment 8

---
+++ src/test/java/usermanagement/entity/PersonTest.java
@@ -0,0 +1,32 @@
+package usermanagement.entity;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PersonTest {
+
+	@Test
+	public void test_person_default_constructor() {
+		Person testClass = new Person();
+		
+		testClass.setmName("mName1");
+		assertEquals("mName1", testClass.getmName());
+
+		testClass.setfName("fName1");
+		assertEquals("fName1", testClass.getfName());
+
+		testClass.setlName("lName1");
+		assertEquals("lName1", testClass.getlName());
+	}
+
+	@Test
+	public void test_set_get_company_name(){
+		Person testClass = new Person();
+
+		testClass.setCompanyName("apple");
+		assertEquals("apple", testClass.getCompanyName());
+	}
+
+
+}
